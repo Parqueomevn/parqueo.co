f43e4668909fda724fcb4982fadabcf6fef718d2
Cristianr <calrod350@gmail.com>
2021-09-18 19:07:10 -0500

    ingresando nuevo entorno de la interfaz

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+   <Headerbar> </Headerbar>
+   <router-view></router-view>
+   <!-- <Login></Login> -->
+   <!-- <Panelmain> </Panelmain> -->
+   <Footer> </Footer>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Headerbar from './components/Headerbar.vue';
+// import Login from './components/Login.vue';
+// import Panelmain from './components/Panelmain.vue';
+import Footer from './components/Footer.vue'
+export default {
+  name: 'App',
+  components: {
+    Headerbar,
+    // Login,
+    // Panelmain
+    Footer
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+/* .login-1{
+    display: flex;
+    justify-content: center;
+    margin-top: 140px;
+} */
+</style>
