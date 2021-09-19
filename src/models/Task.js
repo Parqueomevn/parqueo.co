903b3b9705ba2e1d6a81eb97996b3cd4e47ec2ef
Cristianr <calrod350@gmail.com>
2021-09-18 20:34:33 -0500

    actualizando entorno de desasrrollo

--- src/models/Task.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-// Schema para modelar o tenga un esquema como van a lucir los datos
-const Task = new Schema({
-    title: String,
-    description: String
-});
-// modelo con el nombre que les quier darle
-module.exports = mongoose.model('Task', Task);
